665ef82b34088f7021d5965fac62023b0173acc0
Joyyujia <48073177+Joyyujia@users.noreply.github.com>
2019-05-02 15:08:02 -0700

    Update GameLook.java

--- GameLook.java
+++ GameLook.java
@@ -1,3 +1,4 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -8,6 +9,13 @@ public class GameLook extends JFrame implements Lookconfig{
 	
 	JLabel time;
 
+	public static void main(String[] args) throws InterruptedException {
+		GameLook g = new GameLook();
+		
+		g.showUI();
+		
+	}
+
 	public void showUI() throws InterruptedException {
 		setTitle("Picture Matching");
 		setSize(700,800);
@@ -15,6 +23,38 @@ public class GameLook extends JFrame implements Lookconfig{
 		setLocationRelativeTo(null);
 		setResizable(true);
 		setLayout(null);
+		
+		//add new buttons
+		
+		//start button
+		
+		JButton startJB = new JButton("START");
+		
+		startJB.setBounds(30,700,100,40);
+		
+		startJB.setFocusable(false);
+		
+		this.add(startJB);
+		
+		//rank
+		
+		JButton rank = new JButton("Rank");
+		
+		rank.setBounds(30,100,100,40);
+		
+		this.add(rank);
+		
+		//show remaining time
+		JLabel jl = new JLabel("Time:");
+		
+		jl.setBounds(440,700,80,50);
+		
+		this.add(jl);
+		
+		
+		JLabel timeJl = new JLabel("90");
+		
+		
 		
 		
 		
